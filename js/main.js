86a0a723195ec48ad32c005c4e863cbd6d3d2d5e
Nick <nchemsak@gmail.com>
2018-11-30 14:32:48 -0600

    update:

--- js/main.js
+++ js/main.js
@@ -1,34 +1,30 @@
 // EYE JS
 "use strict";
 
+// $(document).ready(function() {
+
+//     // image info
+//     // width: 1850px / 5 = 370 columns
+//     // height: 1000px / 5 = 200 rows
+//     // 74000 to divs
+
+
+// });
+
+
+
+
+
 $(document).ready(function() {
+    for (var i = 1; i <= 1; i++) {
+        // $('#target').append('<div class="dot-rows" id="dot-row-' + i + '">');
+        for (var j = 1; j <= 18500; j++) {
+            $('#target').append('<div class="all-dots" id="dot-' + j + '"></div>')
 
-    // image info
-    // width: 1850px / 5 = 370 columns
-    // height: 1000px / 5 = 200 rows
-    // 74000 to divs
-
-    var i = 0;
-    var x = 0;
-    for (var x; x <= 7400; x++) {
-        for (var i; i <= 5000; i++) {
-            $('#target').append('<div class="all-dots" id="dot-' + i + '"></div>')
+            // if (j % 370 === 0) {
+            //     $('#target').append('<br />')
+            // }
         }
-            // var nick = x + i;
-            // $('#target').append('<div class="all-dots" id="dot-' + nick + '"></div>')
-            var x = i+x;
+        // $('#target').append('</ div>' );
     }
-
-
-
-
-    // for (var j = 371; j <= 739; j++) {
-    //     $('#target2').append('<div class="all-dots" id="dot-' + j + '"></div>')
-    // }
-
-
-
-
-
-
 });
